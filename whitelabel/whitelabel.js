3f44258e145e9a287b6c4858b19bab62d671de10
CORPORATE\axc001 <andrew.comenzo@perfect.com>
2017-05-24 17:33:22 -0400

    Fixed base URL for new CBB and SBB
    Some path issues related to the new contract board and bid board

--- whitelabel/whitelabel.js
+++ whitelabel/whitelabel.js
@@ -37,9 +37,10 @@ if (inFNC == "whitelabellogin") {
 			_wprocure.push(['_width','700px']);
 }
 
-if (inFNC == "whitelabelcb") {
+if ((inFNC == "contractboard")||(inFNC == "bidboard")) {
 	_baseURL += "/wp-web-public";
-}	
+}
+
 _wprocure.push(['_targetDiv','webprocurediv']);
 _wprocure.push(['_guid', inTOK]);
 _wprocure.push(['_uid', inTOK]);
@@ -50,24 +51,27 @@ _wprocure.push(['_loginProvider', "https://missouribuys.mo.gov/login.html"]);
 _wprocure.push(['_customerid', inEBO]);
 _wprocure.push(['_csspath', _csspath]);
 _wprocure.push(['_vid', _param1]);
+
 		
 (function() {
 	
-	var im = document.createElement('script'); im.type = 'text/javascript';
-
-	if (inFNC == "whitelabelcb") {
-		im.src = _baseURL + "/js/whitlabelutil/whitlabelcbutil.js";
+	var im = document.createElement('script');	im.type = 'text/javascript';
+	var fm = document.createElement('script');	fm.type = 'text/javascript';
+	var s = document.getElementsByTagName('script')[0];
+	
+	if ((inFNC == "contractboard")||(inFNC == "bidboard")) {
+		im.src = _baseURL + "/assets/WhiteLabel.js";
+		fm.src = _baseURL + "/assets/FrameManager.js";
+		
 	} else if (inFNC == "whitelabelreg") {
-		im.src = _baseURL + "/js/supplierreg/loader.js";				
+		im.src = _baseURL + "/js/supplierreg/loader.js";		
+		fm.src = _baseURL + "/js/supplierreg/FrameManager.js";
+		
 	} else {
 		im.src = _baseURL + "/js/whitlabelutil/whitlabelutil.js";
+		fm.src = _baseURL + "/js/whitlabelutil/FrameManager.js";	
 	}
 
-	var fm = document.createElement('script');
-	fm.type = 'text/javascript';
-	fm.src = _baseURL + "/js/whitlabelutil/FrameManager.js";
-
-	var s = document.getElementsByTagName('script')[0];
 	s.parentNode.insertBefore(im, s);
 	s.parentNode.insertBefore(fm, s);
 })();
