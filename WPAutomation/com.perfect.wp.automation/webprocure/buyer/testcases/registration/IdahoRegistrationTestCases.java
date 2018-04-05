6d83ed9a3a7e1597bac5491df395fcad938bc611
anshul.ahuja@instantsys.com <anshul.ahuja@instantsys.com>
2018-04-05 18:20:03 +0530

    WP-2725 : added idaho registration test cases class

--- WPAutomation/com.perfect.wp.automation/webprocure/buyer/testcases/registration/IdahoRegistrationTestCases.java
+++ WPAutomation/com.perfect.wp.automation/webprocure/buyer/testcases/registration/IdahoRegistrationTestCases.java
@@ -1,5 +1,7 @@
 package buyer.testcases.registration;
 
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,78 +17,134 @@ import commonutils.pageobjects.utils.ExtentReport;
 @Listeners(ExtentReport.class)
 public class IdahoRegistrationTestCases {
 
-	IdahowhitelabelPageObjects idaho; 
-	
+	IdahowhitelabelPageObjects idaho;
+
 	@BeforeClass
 	public void setup() {
 		ExtentReport.logger = ExtentReport.report.startTest(this.getClass().getSimpleName());
 		ExtentReport.logger.log(LogStatus.INFO, "Test Case Started");
 		ExtentReport.logger.log(LogStatus.PASS, "Browser Invoked");
 	}
-	
 
-	
 	@BeforeMethod
 	public void geturl() {
-		idaho= new IdahowhitelabelPageObjects();
+		idaho = new IdahowhitelabelPageObjects();
 	}
-	
-	
-	/************Test scripts*****************/
-	
-	
-	@Test(description="This test case will register supplier using SSN and FEIN",enabled=true)
+
+	/************ Test scripts *****************/
+
+	@Test(description = "This test case will register supplier using SSN and FEIN", enabled = true)
 
 	public void registerUsingSsnFein() {
 		ExtentReport.logger.log(LogStatus.INFO, "Test Case Started");
 
-		idaho.firstfintestcase();
+		idaho.clickidahosupplierregistrationbtn();
+		ExtentReport.logger.log(LogStatus.PASS, "Terms And Conditions Accepted");
+
+		idaho.companydetails(true);
+		ExtentReport.logger.log(LogStatus.PASS, "Company Details Entered");
+
+		idaho.Suppliercontactdetail(true);
+		ExtentReport.logger.log(LogStatus.PASS, "Supplier Contact Details Entered");
+
+		idaho.SelectSol();
+		ExtentReport.logger.log(LogStatus.PASS, "Commodities Selected");
+
 		Assert.assertTrue(idaho.verify());
 		ExtentReport.logger.log(LogStatus.PASS, "Register Using Ssn Done");
 
 	}
 
-	
-
-	@Test(description="This test case will register supplier using SSN and FEIN",dependsOnMethods= {"RegisterWithFein"},enabled=true)
-	public void CheckDuplicateFein() {
+	@Test(description = "This test case will register supplier using FEIN", enabled = true)
+	public void RegisterWithFein() {
+		// idaho.Differentfintestcase(true);
 		ExtentReport.logger.log(LogStatus.INFO, "Test Case Started");
 
-		idaho.samefintestcase();
+		idaho.clickidahosupplierregistrationbtn();
+		ExtentReport.logger.log(LogStatus.PASS, "Terms And Conditions Accepted");
 
+		idaho.companydetailstestscript3(true);
+		ExtentReport.logger.log(LogStatus.PASS, "Company Details Entered");
+		idaho.SuppliercontactdetailTestscript3();
+		ExtentReport.logger.log(LogStatus.PASS, "Supplier Contact Details Entered");
+
+		idaho.SelectSol();
+		ExtentReport.logger.log(LogStatus.PASS, "Commodities Selected");
+
+		Assert.assertTrue(idaho.verify());
+		ExtentReport.logger.log(LogStatus.PASS, "Registration Completed");
+
+	}
+
+	@Test(description = "This test case will check if user is able to register using Duplicate Fein", dependsOnMethods = {
+			"RegisterWithFein" }, enabled = true)
+	public void DuplicateFeinCheck() throws IOException {
+		ExtentReport.logger.log(LogStatus.INFO, "Test Case Started");
+		idaho.clickidahosupplierregistrationbtn();
+
+		ExtentReport.logger.log(LogStatus.PASS, "Terms And Conditions Accepted");
+
+		idaho.companydetailstestscript3(false);
+		ExtentReport.logger.log(LogStatus.PASS, "Company Details Entered");
+		// idaho.Differentfintestcase(false);
+		Assert.assertTrue(idaho.verifyDuplicateFein());
 		ExtentReport.logger.log(LogStatus.PASS, "Not able to proceed with Duplicate Fein");
 
 	}
 
-	@Test(description="This test case will register supplier using FEIN",enabled=true)
-	public void RegisterWithFein() {
-		idaho.Differentfintestcase();
-		//Assert.assertTrue(idaho.verify());
-
-
-	}
-	@Test(description="This test case will register supplier using Ssn",enabled=true)
+	@Test(description = "This test case will register supplier using Ssn", enabled = true)
 	public void RegisterUsingSsn() {
-		idaho.firstssntestcase(true);
+		ExtentReport.logger.log(LogStatus.INFO, "Test Case Started");
+
+		idaho.clickidahosupplierregistrationbtn();
+		idaho.companydetailstestscript4(true);
+		ExtentReport.logger.log(LogStatus.PASS, "Company Details Entered");
+
+		idaho.SuppliercontactdetailTestscript4(true);
+		ExtentReport.logger.log(LogStatus.PASS, "Supplier Contact Details Entered");
+
+		idaho.SelectSol();
+		ExtentReport.logger.log(LogStatus.PASS, "Commodities Selected");
+
+		Assert.assertTrue(idaho.verify());
+		ExtentReport.logger.log(LogStatus.PASS, "Registration Completed");
+		// idaho.firstssntestcase(true);
 
 	}
-	
-	@Test(description="This test case will check Duplicate Ssn",dependsOnMethods = {
-	"RegisterUsingSsn" },enabled=true)
+
+	@Test(description = "This test case will check Duplicate Ssn", dependsOnMethods = {
+			"RegisterUsingSsn" }, enabled = true)
 	public void DuplicateSsnCheck() {
-		idaho.firstssntestcase(false);
+		ExtentReport.logger.log(LogStatus.INFO, "Test Case Started");
+
+		idaho.clickidahosupplierregistrationbtn();
+		idaho.companydetailstestscript4(false);
+		// idaho.firstssntestcase(false);
 		Assert.assertTrue(idaho.verifyDuplicateSsn());
 
 	}
-	
-	@Test(description="This test case will check Duplicate Ssn",dependsOnMethods = {
-	"RegisterUsingSsn" },enabled=true)
-	public void DuplicateUsernameCheck() {
-		idaho.DuplicateUsernameCheck();
-		//Assert.assertTrue(idaho.verifyDuplicateSsn());
+
+	@Test(description = "This test case will check Duplicate Ssn", dependsOnMethods = {
+			"RegisterUsingSsn" }, enabled = true)
+	public void DuplicateUsernameCheck() throws IOException {
+		ExtentReport.logger.log(LogStatus.INFO, "Test Case Started");
+
+		// idaho.samefintestcase();
+		idaho.clickidahosupplierregistrationbtn();
+		ExtentReport.logger.log(LogStatus.PASS, "Terms And Conditions Accepted");
+
+		idaho.companydetailstestscript2(false);
+		ExtentReport.logger.log(LogStatus.PASS, "Company Details Entered");
+
+		idaho.SuppliercontactdetailTestscript2(false);
+		ExtentReport.logger.log(LogStatus.PASS, "Supplier Contact Details Entered");
+
+		idaho.SelectSol();
+		ExtentReport.logger.log(LogStatus.PASS, "Commodities Selected");
+		Assert.assertTrue(idaho.verifyDuplicateUsername());
 
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		ExtentReport.report.flush();
