4022cc625686dc05242623064e8823698f1a7ddf
anshulahuja0108 <31890946+anshulahuja0108@users.noreply.github.com>
2018-05-02 15:28:45 +0530

    Update CreateInvoice.java
    WP-2228 : added import for afterClass

--- WPAutomation/com.perfect.wp.automation/webprocure/buyer/testcases/invoice/CreateInvoice.java
+++ WPAutomation/com.perfect.wp.automation/webprocure/buyer/testcases/invoice/CreateInvoice.java
@@ -1,6 +1,7 @@
 package buyer.testcases.invoice;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
