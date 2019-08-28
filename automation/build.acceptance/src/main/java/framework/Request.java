e8083ec4064673e9784c3d26aa458da70969c234
CORPORATE\axc001 <andrew.comenzo@perfect.com>
2019-08-28 16:23:11 -0400

    WP-5052 Automate: View All Requests
    Using data from previous test, look up newly created req and verify information can be accessed and is correct. Also store generated PO for future use.

--- automation/build.acceptance/src/main/java/framework/Request.java
+++ automation/build.acceptance/src/main/java/framework/Request.java
@@ -7,20 +7,22 @@ public class Request {
     private String reqSupplierName;
     private String reqTotal;
     private String reqStatus;
+    private String reqPONumber;
 
     // Setters
-    public void setReqName(String name) { reqName = name; }
-    public void setReqNumber(String number) {reqNumber = number; }
-    public void setReqSupplierName(String name) { reqSupplierName = name; }
-    public void setReqStatus(String status) { reqStatus = status; }
+    public void setReqName(String name) { this.reqName = name; }
+    public void setReqNumber(String number) { this.reqNumber = number; }
+    public void setReqSupplierName(String name) { this.reqSupplierName = name; }
+    public void setReqStatus(String status) { this.reqStatus = status; }
+    public void setReqPONumber(String reqPONumber) { this.reqPONumber = reqPONumber; }
+    public void setReqTotal(String reqTotal) { this.reqTotal = reqTotal; }
 
     // Getters
     public String getReqName() { return reqName; }
     public String getReqNumber() { return reqNumber; }
     public String getReqSupplierName() { return reqSupplierName; }
     public String getReqStatus() { return reqStatus; }
+    public String getReqPONumber() { return reqPONumber; }
+    public String getReqTotal() { return reqTotal; }
 
-    // contains a List of req items
-    // contains private basic info about req (org, name, number, create date, status)
-    // contains getters/setters
 }
--- automation/build.acceptance/src/test/java/testcases/buyer/req/ReqFlowTest.java
+++ automation/build.acceptance/src/test/java/testcases/buyer/req/ReqFlowTest.java
@@ -1,31 +1,84 @@
 package testcases.buyer.req;
 
 import framework.Request;
+import junit.framework.Assert;
+import org.openqa.selenium.WebElement;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import pageobjects.buyer.req.ViewReqPOM;
+import pageobjects.common.BuyerNavBarPOM;
+import pageobjects.common.LoginPagePOM;
+import utilities.common.Browser;
+import utilities.common.ResourceLoader;
 import utilities.common.TestRailListener;
 import utilities.common.TestRailReference;
+
+import java.util.Map;
+
+import static pageobjects.buyer.req.ViewReqPOM.ReqListColumn.*;
 
 @Listeners({TestRailListener.class})
 
 public class ReqFlowTest {
 
     Request request;
+    ResourceLoader resource;
 
     @BeforeClass
     public void setup() {
         request = new Request();
     }
 
+
     @Test
     @TestRailReference(id=3597)
-    public void CreateRequest(ITestContext testContext) {
+    public void CreateRequestTest(ITestContext testContext) {
 
         ReqCreator creator = new ReqCreator();
         request = creator.CreateRequest("data/req", testContext);
     }
 
+    @Test(dependsOnMethods = {"CreateRequestTest"})
+    @TestRailReference(id=3597)
+    public void ViewRequestTest(ITestContext testContext) {
+
+        Browser browser = new Browser(testContext);
+        LoginPagePOM login = new LoginPagePOM(browser);
+        BuyerNavBarPOM navbar = new BuyerNavBarPOM(browser);
+        ViewReqPOM view = new ViewReqPOM(browser);
+
+        resource = new ResourceLoader("data/req");
+        browser.getDriver().get(browser.baseUrl);
+
+        login.loginAsBuyer();
+
+        // Go to Requests > Create New > Off-Catalog Request
+        navbar.selectDropDownItem(resource.getValue("navbar_headitem"), resource.getValue("navbar_viewreq"));
+
+        browser.sendKeysWhenAvailable(view.filterReqNumEdit, request.getReqNumber());
+        browser.clickWhenAvailable(view.filterSubmitButton);
+
+        Map<ViewReqPOM.ReqListColumn, WebElement> reqLine = view.getElementsForReqLine(request.getReqName());
+
+        // make sure req number and status are ok
+        Assert.assertTrue("Verify req number", reqLine.get(REQNUM).getText().contains(request.getReqNumber()));
+        Assert.assertTrue("Verify req status", reqLine.get(STATUS).getText().contains("PO Created"));
+
+        // expand req data and get PO number generated (For further tests)
+        browser.clickSubElement(reqLine.get(EXPAND), view.riDownArrow);
+        browser.waitForElementToAppear(view.riPONumber);
+        request.setReqPONumber(view.riPONumber.getText());
+
+        // make sure total cost is ok
+        Assert.assertTrue("Verify req total", browser.getSubElement(view.reqTable, view.riReqTotal).getText().contains(request.getReqTotal()));
+
+        browser.Log("PO " + request.getReqPONumber() + " created");
+        browser.Log(request.getReqName() + " viewed as a buyer");
+
+        navbar.logout();
+        browser.close();
+    }
 }
 
